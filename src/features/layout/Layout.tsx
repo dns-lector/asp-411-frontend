e9fcfca0dd61d9e50e36144da26230102c8ffb06
dns-lector <157026463+dns-lector@users.noreply.github.com>
2025-12-22 19:20:03 +0200

    first commit

---
+++ src/features/layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+import "./ui/Layout.css";
+
+export default function Layout() {
+
+    return <>
+        <header>Header</header>
+        <main><Outlet /></main>
+        <footer>Footer</footer>
+    </>;
+}
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,8 @@
+import "./ui/Home.css";
+
+export default function Home() {
+
+    return <>
+    <h1>Home</h1>
+    </>;
+}
